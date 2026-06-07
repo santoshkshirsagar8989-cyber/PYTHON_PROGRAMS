c272a4038bdaf337ffdfdef277147b862a7d1078
Santosh Kshirsagar <santoshkshirsagar8989@gmail.com>
2026-06-07 20:56:56 +0530

    virtual environment

---
+++ For virtual environment setup and Flask.py
@@ -0,0 +1,6 @@
+# For virtual environment setup
+# 1.python -m venv my_environment
+# 2.  my_environment/scripts/activate
+print ("For virtual environment setup, follow these steps:")
+print ("1. python -m venv my_environment")
+print ("2.  my_environment/scripts/activate")
