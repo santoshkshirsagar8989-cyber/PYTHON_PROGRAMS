a1db13c705336ba80310008b06fc0188de4ee21e
Santosh Kshirsagar <santoshkshirsagar8989@gmail.com>
2026-06-07 15:20:29 +0530

    list

---
+++ test.py
@@ -0,0 +1,7 @@
+Name = "Santosh"
+Marks = 90
+city = "Hingoli"
+#method 1
+print ("Hello " + Name + "! Your marks are " + str(Marks) + " and you are from " + city)
+#method 2
+print (f"Hello {Name}! Your marks are {Marks} and you are from {city}")
