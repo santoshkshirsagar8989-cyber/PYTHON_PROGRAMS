bb71ee5f7989256118ae69c671ee52f90d319dc2
Santosh Kshirsagar <santoshkshirsagar8989@gmail.com>
2026-07-02 22:05:05 +0530

    ready for deployment

--- database1.py
+++ database1.py
@@ -23,6 +23,8 @@ def init_db():
     conn.commit()
     conn.close()
 
+init_db()
+
 if __name__ == "__main__":
-    init_db()
+
     app.run(debug=True)
--- userdatabase.py
+++ userdatabase.py
@@ -26,6 +26,7 @@ def init_db():
     conn.commit()
     conn.close()
 
+init_db()
+
 if __name__ == "__main__":
-    init_db()
     app.run(debug=True)
